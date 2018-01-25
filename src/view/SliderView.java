a51b0daf93bf8757413945c8240c02b72b8f7585
Kyle Gravenhorst <k.t.c.gravenhorst@me.com>
2018-01-25 12:38:12 +0100

    Cleaned Views; still WIP

--- src/view/SliderView.java
+++ src/view/SliderView.java
@@ -1,12 +1,15 @@
 package view;
 
-import controller.*;
+import controller.Controller;
 
 import javax.swing.*;
+import java.util.Hashtable;
 
 public class SliderView extends JPanel
 {
     private Controller controller;
+
+    private JSlider slider;
 
     /**
      * Constructor for objects of class ButtonView
@@ -14,6 +17,26 @@ public class SliderView extends JPanel
     public SliderView(Controller controller)
     {
         this.controller = controller;
+
+        // Testing the JSlider implementation
+        slider = new JSlider(JSlider.VERTICAL, 11, 1001, 100);
+        add(slider);
+
+        // Setting up the JSlider
+        // Hashtable is needed te add labels to the JSlider
+        Hashtable labelTable = new Hashtable();
+        labelTable.put(1, new JLabel("Fast"));
+        labelTable.put(1000, new JLabel("Slow"));
+
+        slider.setMajorTickSpacing(100);
+        slider.setPaintTicks(true);
+        slider.setLabelTable(labelTable);
+        slider.setPaintLabels(true);
+
+        slider.addChangeListener(event -> {
+            int value = slider.getValue();
+            controller.setTickPause(value);
+        });
 
     }
 }
--- src/view/View.java
+++ src/view/View.java
@@ -1,28 +1,14 @@
 package view;
 
-import controller.*;
+import controller.Controller;
 
 import java.awt.*;
-import java.util.*;
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 public class View extends AbstractView
 {
-    private CarParkView carParkView;
-
     private Controller controller;
 
-    // Create all buttons
-    private JButton start = new JButton("Start");
-    private JButton stop = new JButton("Stop");
-    private JButton minute = new JButton("+1 Minute");
-    private JButton hour = new JButton("+1 Hour");
-    private JButton day = new JButton("+1 Day");
-
-    // Create the JSlider
-    private JSlider slider = new JSlider(JSlider.VERTICAL, 11, 1001, 100);
+    private CarParkView carParkView;
 
     public View()
     {
@@ -42,34 +28,6 @@ public class View extends AbstractView
         SliderView sliderView = new SliderView(controller);
         // TO-DO: finalize a layout
 
-
-        // All buttons need to be added to the buttonView
-        buttonView.add(start);
-        buttonView.add(stop);
-        buttonView.add(minute);
-        buttonView.add(hour);
-        buttonView.add(day);
-
-        // Testing the JSlider implementation
-        sliderView.add(slider);
-
-        // Setting up the JSlider
-        // Hashtable is needed te add labels to the JSlider
-        Hashtable labelTable = new Hashtable();
-        labelTable.put(1, new JLabel("Fast"));
-        labelTable.put(1000, new JLabel("Slow"));
-
-        slider.setMajorTickSpacing(100);
-        slider.setPaintTicks(true);
-        slider.setLabelTable(labelTable);
-        slider.setPaintLabels(true);
-
-        slider.addChangeListener(event -> {
-            int value = slider.getValue();
-            controller.setTickPause(value);
-        });
-
-
         // Create a Container
         Container contentPane = getContentPane();
         // Add all Views
@@ -78,13 +36,6 @@ public class View extends AbstractView
         contentPane.add(sliderView, BorderLayout.EAST);
         // Finalize
         pack();
-
-        // ActionListeners; all lambdas reference the Controller
-        start.addActionListener(e -> controller.start());
-        stop.addActionListener(e -> controller.stop());
-        minute.addActionListener(e -> controller.manualTick(1));
-        hour.addActionListener(e -> controller.manualTick(60));
-        day.addActionListener(e -> controller.manualTick(1440));
 
         // Make everything visible and draw the parking lot
         setVisible(true);
@@ -98,10 +49,6 @@ public class View extends AbstractView
 
     public void updateView(int floors, int rows, int places)
     {
-//        for(CarParkView v: views)
-//        {
-//            v.updateView(floors, rows, places);
-//        }
         carParkView.updateView(floors, rows, places);
     }
 
